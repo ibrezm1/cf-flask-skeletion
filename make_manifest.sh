538ffbfe1cf785c0e923d069ab94f9da031a7ee8
ibrezm1 <ibrezm1@gmail.com>
2021-08-13 09:21:43 +0530

    Readme

--- make_manifest.sh
+++ make_manifest.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 SPACE=$1
-APP_NAME="skeleton-"`git rev-parse --short HEAD`
+# Diff app name in every deployment
+#APP_NAME="skeleton-"`git rev-parse --short HEAD`
+
+#Same appname for deployment
+APP_NAME="skeleton-app"
+
+
 TEMPLATE_MANIFEST_FILE="template-manifest-$SPACE.yml"
 MANIFEST_FILE="manifest.yml"
 
